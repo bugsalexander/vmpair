672fe72a699561538af4683b20f55523cb17e8d5
Alexander Takayama <alex@takayamasan.com>
2021-08-04 21:29:12 -0400

    change to use real server, add install instructions

--- app/common/apiClient.ts
+++ app/common/apiClient.ts
@@ -8,19 +8,22 @@ import {
 } from "./types";
 
 export const ROUTES = {
-  WELCOME: "/api/welcome",
-  PREFERENCES: "/api/preferences",
-  STATS: "/api/stats",
+  WELCOME: "/api/v1/welcome",
+  PREFERENCES: "/api/v1/preferences",
+  STATS: "/api/v1/stats",
 };
 
+const baseURL = "http://localhost:5000";
+
 const ApiClient = {
-  getWelcome: () => axios.get<WelcomeApiResponse>(ROUTES.WELCOME),
-  getPreferences: () => axios.get<PreferencesApiResponse>(ROUTES.PREFERENCES),
-  getStats: () => axios.get<StatsApiResponse>(ROUTES.STATS),
+  getWelcome: () => axios.get<WelcomeApiResponse>(ROUTES.WELCOME, { baseURL }),
+  getPreferences: () =>
+    axios.get<PreferencesApiResponse>(ROUTES.PREFERENCES, { baseURL }),
+  getStats: () => axios.get<StatsApiResponse>(ROUTES.STATS, { baseURL }),
   postWelcome: (payload: WelcomeApiRequest) =>
-    axios.post(ROUTES.WELCOME, payload),
+    axios.post(ROUTES.WELCOME, payload, { baseURL }),
   postPreferences: (payload: PreferencesApiRequest) =>
-    axios.post(ROUTES.PREFERENCES, payload),
+    axios.post(ROUTES.PREFERENCES, payload, { baseURL }),
 };
 
 export default ApiClient;
