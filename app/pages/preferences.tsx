64472b8cc07081a11b9f152ab5220f806e0fa1c4
Alexander Takayama <alex@takayamasan.com>
2021-08-05 06:44:12 -0400

    preferences state works

--- app/pages/preferences.tsx
+++ app/pages/preferences.tsx
@@ -1,8 +1,8 @@
-import { Checkbox, Select, Switch } from "antd";
+import { Button, Checkbox, Select, Switch } from "antd";
 import { Header } from "../components/Header";
 import { FormField } from "../components/FormField";
 import { SelectField } from "../components/SelectField";
-import { ReducerAction, useEffect, useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import ApiClient from "../common/apiClient";
 import {
   DAY,
@@ -15,44 +15,52 @@ import {
 } from "../common/types";
 
 // new reducer type:
-export type DaysReducerState = Record<
-  DAY,
-  {
-    include: boolean;
-    virtual: boolean;
-    inPerson: boolean;
-    t0: boolean;
-    t1: boolean;
-    t2: boolean;
-    t3: boolean;
-    t4: boolean;
-  }
->;
+export type DaysReducerState = {
+  diff: boolean;
+  mapping: Record<
+    DAY,
+    {
+      include: boolean;
+      virtual: boolean;
+      inPerson: boolean;
+      t0: boolean;
+      t1: boolean;
+      t2: boolean;
+      t3: boolean;
+      t4: boolean;
+    }
+  >;
+};
+type MappingFields = keyof DaysReducerState["mapping"][DAY];
 
 function daysFreeReducer(
   state: DaysReducerState | null,
-  payload: { day: DAY; field: keyof DaysReducerState[DAY] } | DaysReducerState
+  payload: { day: DAY; field: MappingFields } | DaysReducerState | "save"
 ) {
   if (state === null) {
     return payload as DaysReducerState | null;
   }
-  const newState = { ...state };
+  if (payload === "save") {
+    return { ...state, diff: false };
+  }
+  const newMapping = { ...state.mapping };
   if ("day" in payload) {
     // make inner shallow copy
-    newState[payload.day] = { ...state[payload.day] };
-    newState[payload.day][payload.field] = !state[payload.day][payload.field];
-    return newState;
+    newMapping[payload.day] = { ...state.mapping[payload.day] };
+    newMapping[payload.day][payload.field] =
+      !state.mapping[payload.day][payload.field];
+    return { mapping: newMapping, diff: true };
   }
   return state;
 }
 
 function initializeDaysFreeReducerState(prefs: PreferencesApiResponse) {
-  const state: DaysReducerState = {} as any;
+  const mapping: DaysReducerState["mapping"] = {} as any;
   for (const currentDay of DAYS_ARR) {
     const dayIndex = prefs.daysFreeToMeet.indexOf(currentDay);
     if (dayIndex != -1) {
       const byDay = prefs.availabilityByDay[dayIndex];
-      state[currentDay] = {
+      mapping[currentDay] = {
         include: true,
         // assume that order of daysFreeToMeet is same day order as availabilityByDay
         virtual: byDay.canVirtual,
@@ -65,7 +73,7 @@ function initializeDaysFreeReducerState(prefs: PreferencesApiResponse) {
       };
     } else {
       // push something blank
-      state[currentDay] = {
+      mapping[currentDay] = {
         include: false,
         virtual: false,
         inPerson: false,
@@ -77,7 +85,7 @@ function initializeDaysFreeReducerState(prefs: PreferencesApiResponse) {
       };
     }
   }
-  return state;
+  return { mapping, diff: false };
 }
 
 const VIRTUAL_STUFF = [
@@ -110,7 +118,12 @@ export default function Preferences() {
       <FormField name="Name:" value={name} onChange={setName} />
       <FormField name="Email:" value={email} onChange={setEmail} />
       <FormField name="Preferred pronouns:" value={pronouns}>
-        <Select className="preferences__input" defaultValue={pronouns}>
+        <Select
+          className="preferences__input"
+          defaultValue={pronouns}
+          value={pronouns}
+          onChange={setPronouns}
+        >
           {Object.entries(PRONOUNS).map(([_key, value]) => (
             <Select.Option key={value} value={value}>
               {value}
@@ -130,11 +143,11 @@ export default function Preferences() {
         dispatch={(d) => daysFreeDispatch({ day: d as DAY, field: "include" })}
         options={DAYS_ARR.map((d) => ({
           name: d,
-          toggled: Boolean(daysFreeToMeet?.[d].include),
+          toggled: Boolean(daysFreeToMeet?.mapping[d].include),
         }))}
       />
       {daysFreeToMeet &&
-        Object.entries(daysFreeToMeet)
+        Object.entries(daysFreeToMeet.mapping)
           .filter(([_, value]) => value.include)
           .sort((a, b) => DAYS[a[0] as DAY] - DAYS[b[0] as DAY])
           .map(
@@ -145,7 +158,7 @@ export default function Preferences() {
                     name={`What times on ${day}?`}
                     options={TIMES_ARR.map((n, i) => ({
                       name: n,
-                      toggled: (daysFreeToMeet as any)[day]["t" + i],
+                      toggled: (daysFreeToMeet?.mapping as any)[day]["t" + i],
                     }))}
                     dispatch={(time) => {
                       const index = TIMES_ARR.indexOf(time);
@@ -154,13 +167,40 @@ export default function Preferences() {
                     className="preferences__selectdays__select"
                   />
                   <div className="preferences__selectdays__mode">
-                    <Checkbox>Virtual</Checkbox>
+                    <Checkbox
+                      checked={(daysFreeToMeet.mapping as any)[day].virtual}
+                      onClick={() =>
+                        daysFreeDispatch({ day, field: "virtual" } as any)
+                      }
+                    >
+                      Virtual
+                    </Checkbox>
                     <br />
-                    <Checkbox>In-person</Checkbox>
+                    <Checkbox
+                      checked={(daysFreeToMeet.mapping as any)[day].inPerson}
+                      onClick={() =>
+                        daysFreeDispatch({ day, field: "inPerson" } as any)
+                      }
+                    >
+                      In-person
+                    </Checkbox>
                   </div>
                 </div>
               )
           )}
+      <div className="preferences__buttonflex">
+        <Button
+          className="preferences__buttonflex__button"
+          type="primary"
+          disabled={!daysFreeToMeet?.diff}
+          onClick={() => {
+            console.log(daysFreeToMeet);
+            daysFreeDispatch("save");
+          }}
+        >
+          Save my preferences
+        </Button>
+      </div>
     </>
   );
 }
